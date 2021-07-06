8b6d4dacecc3bbffddbb51adfaa744fd3f6019c8
wstaniszewski <w.staniszewski@gmail.com>
2021-07-06 09:33:11 +0200

    duzo zmian - różne warianty

--- main.ts
+++ main.ts
@@ -1,13 +1,46 @@
-let strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
-strip.setBrightness(10)
-let range = strip.range(0, 6)
+input.onButtonPressed(Button.A, function () {
+    jasnosc = 10
+    strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
+    strip.setBrightness(jasnosc)
+    range = strip.range(0, 12)
+    range.showRainbow(1, 320)
+    serial.writeLine("Connected")
+})
+input.onButtonPressed(Button.AB, function () {
+    jasnosc = 2
+    strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
+    strip.setBrightness(jasnosc)
+    range = strip.range(0, 24)
+    range.showRainbow(1, 320)
+    serial.writeLine("Connected")
+})
+input.onButtonPressed(Button.B, function () {
+    jasnosc = 150
+    strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
+    strip.setBrightness(jasnosc)
+    range = strip.range(0, 12)
+    range.showRainbow(1, 320)
+    serial.writeLine("Connected")
+})
+input.onGesture(Gesture.Shake, function () {
+    jasnosc = 150
+    strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
+    strip.setBrightness(jasnosc)
+    range = strip.range(0, 1)
+    range.showRainbow(1, 320)
+    serial.writeLine("Connected")
+})
+let range: neopixel.Strip = null
+let strip: neopixel.Strip = null
+let jasnosc = 0
+jasnosc = 50
+strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
+strip.setBrightness(jasnosc)
+range = strip.range(0, 12)
 range.showRainbow(1, 320)
-let range2 = strip.range(8, 6)
-range2.showRainbow(1, 320)
-let range3 = strip.range(16, 6)
-range3.showRainbow(1, 320)
-serial.writeLine("Connected!")
+serial.writeLine("Connected")
 basic.forever(function () {
+    strip.setBrightness(jasnosc)
     strip.rotate(1)
     basic.pause(100)
     strip.show()
